da74ef7cd0ed79c5db2f9465db56c0b6da0f2c86
pagzamaphone <anthony.pagnotta@gmail.com>
2020-11-09 23:31:21 -0500

    Add files via upload

---
+++ Counting Semaphore Example/Reader.java
@@ -0,0 +1,18 @@
+package whatever;
+
+public class Reader implements Runnable {
+	private Library library;
+	public Reader(Library library) {
+		this.library = library;
+	}
+	@Override
+	public void run() {
+		try {
+			Book book = (Book)library.issueBook();
+			book.read();
+			library.returnBook(book);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+} 
